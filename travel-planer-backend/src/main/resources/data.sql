b52049f54ee0114817d91fa24dca354d8c92bb47
Filip Savic <filipsavic1995@yahoo.com>
2019-07-02 19:02:00 +0200

    Fixed data.sql entries.

--- travel-planer-backend/src/main/resources/data.sql
+++ travel-planer-backend/src/main/resources/data.sql
@@ -1,6 +1,10 @@
-insert into object (id,name,location_id,type,rating) values (1, 'Giuseppe Meazza', 1, 1,4.5);
-insert into object (id,name,location_id,type,rating) values (2, 'Hotel Park', 2, 0, 3.2);
-insert into object (id,name,location_id,type,rating) values (3, 'Colosseum', 1, 6, 3.5);
-insert into object (id,name,location_id,type,rating) values (4, 'Pantheon', 1, 6, 4.5);
-insert into object (id,name,location_id,type,rating) values (5, 'Promenada', 2, 7, 2.3);
-insert into object (id,name,location_id,type,rating) values (6, 'Vesuvius', 3, 3, 5.0);
+insert into location (id, longitude, latitude, city, country) values (1001, 12.496365, 41.902782, 'Rome', 'Italy'),
+  (1002, 19.833549, 45.267136, 'Novi Sad', 'Serbia'),
+  (1003, 14.268120, 40.851799, 'Naples', 'Italy');
+-- --
+insert into object (id,name,location_id,type,rating) values (1, 'Giuseppe Meazza', 1001, 1,4.5);
+insert into object (id,name,location_id,type,rating) values (2, 'Hotel Park', 1002, 0, 3.2);
+insert into object (id,name,location_id,type,rating) values (3, 'Colosseum', 1001, 6, 3.5);
+insert into object (id,name,location_id,type,rating) values (4, 'Pantheon', 1001, 6, 4.5);
+insert into object (id,name,location_id,type,rating) values (5, 'Promenada', 1002, 7, 2.3);
+insert into object (id,name,location_id,type,rating) values (6, 'Vesuvius', 1003, 3, 5.0);
